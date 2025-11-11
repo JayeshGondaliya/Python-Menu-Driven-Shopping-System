5ca1168d859c763407b9f23a481b05f1bfefd665
Jayesh Gondaliya <166483981+JayeshGondaliya@users.noreply.github.com>
2025-11-11 19:24:51 +0530

    ok

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def display_product():
         return
 
     print("\n" + "="*45)
-    print(" AVAILABLE PRODUCTS")
+    print("🛍️ AVAILABLE PRODUCTS")
     print("="*45)
     print(f"{'Product ID':<12}{'Product Name':<20}{'Price (₹)':<10}")
     print("-"*45)
